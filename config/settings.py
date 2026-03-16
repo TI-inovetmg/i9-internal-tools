6cc3ee6ababe20ea600f1fe0190bdb59a753fa18
Vinicius Stoc <vmartinsstoc@gmail.com>
2026-03-16 09:41:53 -0300

    UPLOAD/ Correção modo debug

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['inovetmg.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['iNoveTmg.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
